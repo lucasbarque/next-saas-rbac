00a9a3aea189113ae62a5095350f37b07c36bb0b
Lucas Barque <lucasbarquedasilva@hotmail.com>
2025-08-09 15:31:01 -0400

    feat: integrate Fastify Swagger for API documentation and add Swagger UI for enhanced accessibility

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,17 +1,35 @@
 import { fastify } from 'fastify'
 import fastifyCors from '@fastify/cors'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import {
-  // jsonSchemaTransform,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { createAccount } from './routes/auth/create-account'
+import fastifySwagger from '@fastify/swagger'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js Saas',
+      description: 'Full-stack Saas app with multi-tenant & RBAC.',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 app.register(fastifyCors)
 
