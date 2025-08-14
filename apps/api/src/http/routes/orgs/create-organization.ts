7c0254303acb5c0241cd4c599e5f3bcc0c89bbb6
Lucas Barque <lucasbarquedasilva@hotmail.com>
2025-08-13 23:57:34 -0400

    feat: implement create organization route with validation and user association

---
+++ apps/api/src/http/routes/orgs/create-organization.ts
@@ -0,0 +1,72 @@
+import { prisma } from '@/lib/prisma'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { hash } from 'bcryptjs'
+import { BadRequestError } from '../_errors/bad-request-error'
+import { auth } from '@/http/middlewares/auth'
+import { domain } from 'node_modules/zod/v4/core/regexes.cjs'
+import { createSlug } from '@/utils/create-slug'
+
+export async function createOrganization(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/organizations',
+      {
+        schema: {
+          tags: ['Organizations'],
+          summary: 'Create a new organization',
+          security: [{ bearerAuth: [] }],
+          body: z.object({
+            name: z.string(),
+            domain: z.string().nullish(),
+            shouldAttachUsersByDomain: z.boolean().optional(),
+          }),
+          response: {
+            201: z.object({
+              organizationId: z.uuid(),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+
+        const { name, domain, shouldAttachUsersByDomain } = request.body
+
+        if (domain) {
+          const organizationByDomain = await prisma.organization.findUnique({
+            where: {
+              domain,
+            },
+          })
+
+          if (organizationByDomain) {
+            throw new BadRequestError('Another organization with same domain already exists')
+          }
+        }
+
+        const organization = await prisma.organization.create({
+          data: {
+            name,
+            slug: createSlug(name),
+            domain,
+            shouldAttachUsersByDomain,
+            ownerId: userId,
+            members: {
+              create: {
+                userId,
+                role: 'ADMIN',
+              },
+            },
+          },
+        })
+
+        return reply.status(201).send({
+          organizationId: organization.id,
+        })
+      }
+    )
+}
