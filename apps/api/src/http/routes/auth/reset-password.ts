63e44809f528b1c6842ae4d1f7b2befd7a78aa27
Lucas Barque <lucasbarquedasilva@hotmail.com>
2025-08-22 23:54:36 -0400

    refactor: reorganize imports and enhance password reset logic by updating token handling and improving transaction management

--- apps/api/src/http/routes/auth/reset-password.ts
+++ apps/api/src/http/routes/auth/reset-password.ts
@@ -1,16 +1,17 @@
-import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
-import z from 'zod'
-import { UnauthorizedError } from '../_errors/unauthorized-error'
-import { hash } from 'bcryptjs'
+import { z } from 'zod'
+
+import { UnauthorizedError } from '@/http/routes/_errors/unauthorized-error'
+import { prisma } from '@/lib/prisma'
 
 export async function resetPassword(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
     '/password/reset',
     {
       schema: {
-        tags: ['auth'],
+        tags: ['Auth'],
         summary: 'Get authenticated user profile',
         body: z.object({
           code: z.string(),
@@ -24,7 +25,7 @@ export async function resetPassword(app: FastifyInstance) {
     async (request, reply) => {
       const { code, password } = request.body
 
-      const tokenFromCode = await prisma.user.findUnique({
+      const tokenFromCode = await prisma.token.findUnique({
         where: { id: code },
       })
 
@@ -34,12 +35,21 @@ export async function resetPassword(app: FastifyInstance) {
 
       const passwordHash = await hash(password, 6)
 
-      await prisma.user.update({
-        where: { id: tokenFromCode.id },
-        data: {
-          passwordHash,
-        },
-      })
+      await prisma.$transaction([
+        prisma.user.update({
+          where: {
+            id: tokenFromCode.userId,
+          },
+          data: {
+            passwordHash,
+          },
+        }),
+        prisma.token.delete({
+          where: {
+            id: code,
+          },
+        }),
+      ])
 
       return reply.status(204).send()
     }
