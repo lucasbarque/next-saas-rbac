b42df1c8fb2badcdc36ed16597e6a50cd79efd62
Lucas Barque <lucasbarquedasilva@hotmail.com>
2025-08-22 23:54:24 -0400

    refactor: improve import organization, enhance email validation, and update error message in create-account route

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,20 +1,21 @@
-import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
-import { hash } from 'bcryptjs'
-import { BadRequestError } from '../_errors/bad-request-error'
+
+import { BadRequestError } from '@/http/routes/_errors/bad-request-error'
+import { prisma } from '@/lib/prisma'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
     '/users',
     {
       schema: {
-        tags: ['auth'],
+        tags: ['Auth'],
         summary: 'Create a new account',
         body: z.object({
           name: z.string(),
-          email: z.email(),
+          email: z.string().email(),
           password: z.string().min(6),
         }),
       },
@@ -23,14 +24,17 @@ export async function createAccount(app: FastifyInstance) {
       const { name, email, password } = request.body
 
       const userWithSameEmail = await prisma.user.findUnique({
-        where: { email },
+        where: {
+          email,
+        },
       })
 
       if (userWithSameEmail) {
-        throw new BadRequestError('User with same email already exists.')
+        throw new BadRequestError('User with same e-mail already exists.')
       }
 
       const [, domain] = email.split('@')
+
       const autoJoinOrganization = await prisma.organization.findFirst({
         where: {
           domain,
