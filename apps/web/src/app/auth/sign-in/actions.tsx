91beef95b1685c12ae15e60475ff874125f5be93
Lucas Barque <lucasbarquedasilva@hotmail.com>
2025-08-23 16:20:41 -0400

    feat: implement sign-in action for email and password authentication with simulated delay

---
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -0,0 +1,19 @@
+'use server'
+
+import { signInWithPassword } from '@/http/sign-in-with-password'
+
+export async function signInWithEmailAndPassword(previousState: any, data: FormData) {
+  console.log(previousState)
+  const { email, password } = Object.fromEntries(data)
+
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  const result = await signInWithPassword({
+    email: String(email),
+    password: String(password),
+  })
+
+  return 'Sucesso'
+
+  console.log(result)
+}
