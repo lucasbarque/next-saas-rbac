51f4d81d5f9725b8a0c3548765d77d40fec15081
Lucas Barque <lucasbarquedasilva@hotmail.com>
2025-08-22 23:54:33 -0400

    refactor: reorganize imports, enhance email validation, and improve comments in request-password-recover route

--- apps/api/src/http/routes/auth/request-password-recover.ts
+++ apps/api/src/http/routes/auth/request-password-recover.ts
@@ -1,17 +1,18 @@
-import { prisma } from '@/lib/prisma'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
-import z from 'zod'
+import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
 
 export async function requestPasswordRecover(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
     '/password/recover',
     {
       schema: {
-        tags: ['auth'],
+        tags: ['Auth'],
         summary: 'Get authenticated user profile',
         body: z.object({
-          email: z.email(),
+          email: z.string().email(),
         }),
         response: {
           201: z.null(),
@@ -26,7 +27,7 @@ export async function requestPasswordRecover(app: FastifyInstance) {
       })
 
       if (!userFromEmail) {
-        // We don't want people to know if user really exists
+        // We don't want to people to know if the user really exists
         return reply.status(201).send()
       }
 
@@ -37,7 +38,9 @@ export async function requestPasswordRecover(app: FastifyInstance) {
         },
       })
 
-      console.log('Recover password token: ', code)
+      // Send e-mail with password recover link
+
+      console.log('Password recover token:', code)
 
       return reply.status(201).send()
     }
